8ce8354c5a534206b6ae6467f67f4bfc922f085a
owner <>
2020-05-02 21:40:00 +0300

    updated

--- conftest.py
+++ conftest.py
@@ -5,19 +5,22 @@ def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
                      help="Choose browser language")
 
-
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("language")
+    language = request.config.getoption("language")
     # browser = None
-    if browser_name == "es":
-        print("\nstart chrome browser for test..")
+    if language == "es":
         browser = webdriver.Chrome()
         browser.get("http://selenium1py.pythonanywhere.com/es/catalogue/coders-at-work_207/")
-    elif browser_name == "ru":
-        print("\nstart firefox browser for test..")
+    elif language == "ru":
         browser = webdriver.Chrome()
         browser.get("http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/")
+    elif language == "fr":
+        browser = webdriver.Chrome()
+        browser.get("http://selenium1py.pythonanywhere.com/fr/catalogue/coders-at-work_207/")
+    elif language == "de":
+        browser = webdriver.Chrome()
+        browser.get("http://selenium1py.pythonanywhere.com/de/catalogue/coders-at-work_207/")
     else:
         raise pytest.UsageError("--language name should be indicated")
     yield browser
--- test_items.py
+++ test_items.py
@@ -1,3 +1,5 @@
+import time
 
 def test_guest_should_see_add_to_basket_button(browser):
+    time.sleep(30)
     browser.find_element_by_class_name("btn-add-to-basket")
