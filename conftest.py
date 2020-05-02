6ad1a417e75388bf276d109600298ac161ec437a
owner <>
2020-05-02 21:03:53 +0300

    logic added

--- conftest.py
+++ conftest.py
@@ -1,8 +1,25 @@
 import pytest
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose browser language")
+
+
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test...")
-    browser = webdriver.Chrome()
+def browser(request):
+    browser_name = request.config.getoption("language")
+    # browser = None
+    if browser_name == "es":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+        browser.get("http://selenium1py.pythonanywhere.com/es/catalogue/coders-at-work_207/")
+    elif browser_name == "ru":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Chrome()
+        browser.get("http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/")
+    else:
+        raise pytest.UsageError("--language name should be indicated")
     yield browser
     print("\nquit browser..")
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,1 +1,3 @@
-import pytest
+
+def test_guest_should_see_add_to_basket_button(browser):
+    browser.find_element_by_class_name("btn-add-to-basket")
